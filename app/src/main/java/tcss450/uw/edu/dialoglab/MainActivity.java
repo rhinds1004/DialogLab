c817e60c1ce3f2e5028307bb6a583ed76396dcd2
rhinds1004 <rhinds1004@gmail.com>
2017-04-14 18:27:06 -0700

    added notification into main activity.

--- app/src/main/java/tcss450/uw/edu/dialoglab/MainActivity.java
+++ app/src/main/java/tcss450/uw/edu/dialoglab/MainActivity.java
@@ -1,6 +1,12 @@
 package tcss450.uw.edu.dialoglab;
 
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
 import android.support.v4.app.DialogFragment;
+import android.support.v4.app.NotificationCompat;
+import android.support.v4.app.TaskStackBuilder;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -30,7 +36,38 @@ public class MainActivity extends AppCompatActivity {
         else if(view.getId() == R.id.time_button){
             fragment = new TimePickerDialogFragment();
         }
+        else if(view.getId() == R.id.date_button){
+            fragment = new DatePickerDialogFragment();
+        }
+        else if(view.getId() == R.id.notification_button){
+            NotificationCompat.Builder mBuilder =
+                    new NotificationCompat.Builder(this)
+                            .setSmallIcon(R.drawable.notification_bg)
+                            .setContentTitle("My notification")
+                            .setContentText(Integer.valueOf(this.getTaskId()).toString());
+// Creates an explicit intent for an Activity in your app
+            Intent resultIntent = new Intent(this, MainActivity.class);
 
+// The stack builder object will contain an artificial back stack for the
+// started Activity.
+// This ensures that navigating backward from the Activity leads out of
+// your application to the Home screen.
+            TaskStackBuilder stackBuilder = TaskStackBuilder.create(this);
+// Adds the back stack for the Intent (but not the Intent itself)
+            stackBuilder.addParentStack(MainActivity.class);
+// Adds the Intent that starts the Activity to the top of the stack
+            stackBuilder.addNextIntent(resultIntent);
+            PendingIntent resultPendingIntent =
+                    stackBuilder.getPendingIntent(
+                            0,
+                            PendingIntent.FLAG_UPDATE_CURRENT
+                    );
+            mBuilder.setContentIntent(resultPendingIntent);
+            NotificationManager mNotificationManager =
+                    (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+// mId allows you to update the notification later on.
+            mNotificationManager.notify(this.getTaskId(), mBuilder.build());
+        }
         if(fragment != null)
             fragment.show(getSupportFragmentManager(), "launch");
     }
